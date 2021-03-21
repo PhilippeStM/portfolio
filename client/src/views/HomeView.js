f7ceedb16f4250616adc2fa80f3be1f419965473
phil-stm <philippestm@yahoo.com>
2021-03-21 13:54:27 -0700

    Did CSS on navbar and logo/about me text to make it centered, started to work on the about me text and now working on making 2 columns for the skills and other info. Likely flexbox.

--- client/src/views/HomeView.js
+++ client/src/views/HomeView.js
@@ -7,13 +7,28 @@ const HomeView = () => {
 
 
     return (
-
         <main>
-        <NavBarHome />
-            <section className={styles.blackBar}>
+            <NavBarHome />
+            <section>
                 <img src={Profile} alt="My profile" className={styles.profilePic}></img>
+                <section className={styles.bar}>
+                    <h2 className={styles.myStoryH2}>About Me...</h2>
+                </section>
             </section>
+            <p className={styles.aboutMeText}>My first experience with coding was in 5th grade in computer class, where we learned to create simple HTML webpages. Since then, I've always had an appreciation of the technology and what it can do. Combined with my strong sense of design and color use. I have so much experience working with and helping others. I've had the opportunity to save lives as a 911 dispatcher, manage a theater, educate young children. I am transitioning into tech and want to bring my life/work experiences, continue to help others and design awesome front end applications. I have always enjoyed seeing the big picture, relentless pursuing understanding of all the moving pieces, then making it all come together.</p>
+
+            <div className={styles.row}>
+                <section className={styles.column}>
+                    <p>test</p>
+                </section>
+                <section className={styles.column}>
+                    <p>test</p>
+                </section>
+            </div>
         </main>
+
+
+
 
     )
 }
