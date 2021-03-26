cef87ff44378985d76a92acfc498abe1094638a2
phil-stm <philippestm@yahoo.com>
2021-03-26 14:26:56 -0700

    Finished coding the experience accordian effects with if/else statements. Now functional. It will change colors when clicked and stay that color until a new accordian is selected. The code will probably need to be refarctored in the future.

--- client/src/components/experienceView/ExperienceTimeline.js
+++ client/src/components/experienceView/ExperienceTimeline.js
@@ -7,7 +7,7 @@ const ExperienceTimeline = () => {
 
     // This is the function and state for CUSTOMER SERVICE
 
-    const [newColorCustomerService, setNewColorCustomerService] = useState("orangered");
+    const [newColorCustomerService, setNewColorCustomerService] = useState("rgba(241, 172, 93, 0.705)");
     const [activeCustomerService, setActiveCustomerService] = useState(false);
 
     const changeColorCustomerService = () => {
@@ -16,14 +16,20 @@ const ExperienceTimeline = () => {
         setActiveDispatcher(false);
         setNewColorPSO("rgba(148, 137, 247, 0.24)");
         setActivePSO(false);
+        setNewColorManager("#71909033");
+        setActiveManager(false);
+        setNewColorChess("rgba(148, 137, 247, 0.24");
+        setActiveChess(false);
+        setNewColorMultiple("#71909033");
+        setActiveMultiple(false);
 
         if (activeCustomerService === true && newColorCustomerService === "rgba(148, 137, 247, 0.24") {
             setNewColorCustomerService("rgba(148, 137, 247, 0.24");
             setActiveCustomerService(false);
         }
-        else{
+        else {
             setActiveCustomerService(true);
-            setNewColorCustomerService("orangered");
+            setNewColorCustomerService("rgba(241, 172, 93, 0.705)");
         }
     }
 
@@ -40,6 +46,12 @@ const ExperienceTimeline = () => {
         setActiveCustomerService(false);
         setNewColorPSO("rgba(148, 137, 247, 0.24)");
         setActivePSO(false);
+        setNewColorManager("#71909033");
+        setActiveManager(false);
+        setNewColorChess("rgba(148, 137, 247, 0.24");
+        setActiveChess(false);
+        setNewColorMultiple("#71909033");
+        setActiveMultiple(false);
 
         if (activeDispatcher === true && newColorDispatcher === "#71909033") {
             setNewColorDispatcher("#71909033");
@@ -47,7 +59,7 @@ const ExperienceTimeline = () => {
         }
         else {
             setActiveDispatcher(true);
-            setNewColorDispatcher("orangered");
+            setNewColorDispatcher("rgba(241, 172, 93, 0.705)");
         }
     }
 
@@ -64,18 +76,113 @@ const ExperienceTimeline = () => {
         setActiveCustomerService(false);
         setNewColorDispatcher("#71909033");
         setActiveDispatcher(false);
+        setNewColorManager("#71909033");
+        setActiveManager(false);
+        setNewColorChess("rgba(148, 137, 247, 0.24");
+        setActiveChess(false);
+        setNewColorMultiple("#71909033");
+        setActiveMultiple(false);
 
-        if (activePSO === true) {
+        if (activePSO === true && newColorPSO === "rgba(148, 137, 247, 0.24") {
             setNewColorPSO("rgba(148, 137, 247, 0.24)");
             setActivePSO(false);
         }
         else {
             setActivePSO(true);
-            setNewColorPSO("orangered");
+            setNewColorPSO("rgba(241, 172, 93, 0.705)");
         }
     }
 
     // **************************************************************
+
+    // This is the function and state for MANAGER
+
+    const [newColorManager, setNewColorManager] = useState("");
+    const [activeManager, setActiveManager] = useState(false);
+
+    const changeColorManager = () => {
+
+        setNewColorCustomerService("rgba(148, 137, 247, 0.24");
+        setActiveCustomerService(false);
+        setNewColorDispatcher("#71909033");
+        setActiveDispatcher(false);
+        setNewColorPSO("rgba(148, 137, 247, 0.24");
+        setActivePSO(false);
+        setNewColorChess("rgba(148, 137, 247, 0.24");
+        setActiveChess(false);
+        setNewColorMultiple("#71909033");
+        setActiveMultiple(false);
+
+        if (activeManager === true && newColorManager === "#71909033") {
+            setNewColorManager("#71909033");
+            setActiveManager(false);
+        }
+        else {
+            setActiveManager(true);
+            setNewColorManager("rgba(241, 172, 93, 0.705)");
+        }
+    }
+
+    // **************************************************************
+
+    // This is the function and state for CHESS
+
+    const [newColorChess, setNewColorChess] = useState("");
+    const [activeChess, setActiveChess] = useState(false);
+
+    const changeColorChess = () => {
+
+        setNewColorCustomerService("rgba(148, 137, 247, 0.24");
+        setActiveCustomerService(false);
+        setNewColorDispatcher("#71909033");
+        setActiveDispatcher(false);
+        setNewColorPSO("rgba(148, 137, 247, 0.24");
+        setActivePSO(false);
+        setNewColorManager("#71909033");
+        setActiveManager(false);
+        setNewColorMultiple("#71909033");
+        setActiveMultiple(false);
+
+        if (activeChess === true && newColorChess === "rgba(148, 137, 247, 0.24") {
+            setNewColorChess("rgba(148, 137, 247, 0.24");
+            setActiveChess(false);
+        }
+        else {
+            setActiveChess(true);
+            setNewColorChess("rgba(241, 172, 93, 0.705)");
+        }
+    }
+
+    // **************************************************************  
+
+    // This is the function and state for MULTIPLE POSITIONS
+
+    const [newColorMultiple, setNewColorMultiple] = useState("");
+    const [activeMultiple, setActiveMultiple] = useState(false);
+
+    const changeColorMultiple = () => {
+        setNewColorCustomerService("rgba(148, 137, 247, 0.24");
+        setActiveCustomerService(false);
+        setNewColorDispatcher("#71909033");
+        setActiveDispatcher(false);
+        setNewColorPSO("rgba(148, 137, 247, 0.24");
+        setActivePSO(false);
+        setNewColorManager("#71909033");
+        setActiveManager(false);
+        setNewColorChess("rgba(148, 137, 247, 0.24");
+        setActiveChess(false);
+
+        if (activeMultiple === true && newColorMultiple === "#71909033") {
+            setNewColorMultiple("#71909033");
+            setActiveMultiple(false);
+        }
+        else {
+            setActiveMultiple(true);
+            setNewColorMultiple(("rgba(241, 172, 93, 0.705)"));
+        }
+    }
+
+    // **************************************************************  
 
     return (
         <div>
@@ -126,7 +233,10 @@ const ExperienceTimeline = () => {
                     </Accordion.Collapse>
                 </Card>
 
-                <Card className={styles.accordianCardGreen}>
+                <Card className={styles.accordianCardGreen}
+                    onClick={changeColorManager}
+                    style={{ backgroundColor: newColorManager }}
+                >
                     <Accordion.Toggle as={Card.Header} eventKey="3">
                         <span className={styles.title}>Assistant Manager</span>
                     </Accordion.Toggle>
@@ -138,7 +248,10 @@ const ExperienceTimeline = () => {
                     </Accordion.Collapse>
                 </Card>
 
-                <Card className={styles.accordianCardPurple}>
+                <Card className={styles.accordianCardPurple}
+                    onClick={changeColorChess}
+                    style={{ backgroundColor: newColorChess }}
+                >
                     <Accordion.Toggle as={Card.Header} eventKey="4">
                         <span className={styles.title}>Chess Instructor</span>
                     </Accordion.Toggle>
@@ -150,7 +263,10 @@ const ExperienceTimeline = () => {
                     </Accordion.Collapse>
                 </Card>
 
-                <Card className={styles.accordianCardGreen}>
+                <Card className={styles.accordianCardGreen}
+                    onClick={changeColorMultiple}
+                    style={{ backgroundColor: newColorMultiple }}
+                >
                     <Accordion.Toggle as={Card.Header} eventKey="5">
                         <span className={styles.title}>Multiple Positions (Cinemark)</span>
                     </Accordion.Toggle>
