f6c9b0df2eb8e7e3961c79f0904c50fceb07fecc
phil-stm <philippestm@yahoo.com>
2021-03-26 21:09:25 -0700

    finished adding hyperlink that opens in new window for github page on the learn more popover.

--- client/src/components/projectView/ProjectPopover.js
+++ client/src/components/projectView/ProjectPopover.js
@@ -7,11 +7,13 @@ const ProjectPopover = () => {
 
     const popover = (
         <Popover id="popover-basic">
-            <Popover.Title as="h3">Popover right</Popover.Title>
+            <Popover.Title as="h3">Portfolio Features:</Popover.Title>
             <Popover.Content>
-                And here's some <strong>amazing</strong> content. It's very engaging.
-        right?
-    </Popover.Content>
+                This webpage was created using the <strong>React</strong> framework, which includes extensive use of <strong>JavaScript/jQuery</strong> and some <strong>HTML/CSS</strong>. <br></br>
+                <br></br>
+                <strong>React Bootstrap</strong> was used to create many of the features, including the navbar, cards, and the accordian effects on the experience view.
+            </Popover.Content>
+            <Popover.Title as="h3"><a href="https://github.com/PhilippeStM/portfolio" target="_blank" rel="noreferrer">View on GitHub.</a></Popover.Title>
         </Popover>
     );
 
