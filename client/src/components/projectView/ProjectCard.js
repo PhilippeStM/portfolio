5d623df5a9d84d0f2fc8ba97fac21eb1cb107fd9
phil-stm <philippestm@yahoo.com>
2021-03-27 11:35:56 -0700

    deployed and refactored clothing emporium and added link / info to this project into my portfolio project view. Added additional props to the project card. Created first card for clothing emporium.

--- client/src/components/projectView/ProjectCard.js
+++ client/src/components/projectView/ProjectCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import styles from '../../styles/ProjectCard.module.css';
 
 const ProjectCard = (props) => {
     return (
@@ -10,13 +11,17 @@ const ProjectCard = (props) => {
         imgSrc={props.imgSrc}
         title={props.title}
         text={props.text}
-        buttonText={props.buttonText}        
+        techUsed={props.techUsed}
+        buttonText={props.buttonText}
+        href={props.href}        
         >
-            <Card.Img variant="top" src={props.imgSrc} />
+            <Card.Img variant="top" src={props.imgSrc} className={styles.img}/>
             <Card.Body>
                 <Card.Title>{props.title}</Card.Title>
                 <Card.Text>{props.text}</Card.Text>
-                <Button variant="primary">{props.buttonText}</Button>
+                <Card.Text><u>Technologies Used:</u></Card.Text>
+                <Card.Text><b>{props.techUsed}</b></Card.Text>
+                <Button variant="primary" href={props.href} target="_blank" rel="noreferrer">{props.buttonText}</Button>
             </Card.Body>
         </Card>
     )
--- client/src/components/projectView/ProjectViewTabs.js
+++ client/src/components/projectView/ProjectViewTabs.js
@@ -4,21 +4,24 @@ import Tabs from 'react-bootstrap/Tabs';
 import CardDeck from 'react-bootstrap/CardDeck';
 import ProjectCard from '../projectView/ProjectCard';
 import TestLogo from '../../images/css-logo.png';
+import ClothingEmporiumPicture from '../../images/clothing-emporium.png';
 import styles from '../../styles/ProjectsView.module.css';
 
 const ProjectViewTabs = () => {
 
     return (
         <div className={styles.tabContainer}>
-            <Tabs fill defaultActiveKey="home" id="uncontrolled-tab-example" className={styles.tabLinkContainer}>
+            <Tabs fill justify defaultActiveKey="home" id="uncontrolled-tab-example" className={styles.tabLinkContainer}>
                 <Tab eventKey="home" title="Featured">
                     <CardDeck className={styles.cardDeckContainer}>
                         <ProjectCard
-                            id="bootcamp"
-                            imgSrc={TestLogo}
-                            title="test title"
-                            text="test text"
-                            buttonText="test button"
+                            id="featured"
+                            imgSrc={ClothingEmporiumPicture}
+                            title="Clothing Emporium"
+                            text="Capstone Project for TekSystems Bootcamp. Created with React framework, implemented useContext to save state on the cart."
+                            techUsed="React, React Bootstrap, JavaScript, jQuery, HTML, CSS."
+                            buttonText="View Project"
+                            href="http://flashy-kite.surge.sh/"
                         />
                         <ProjectCard
                             id="bootcamp"
@@ -26,6 +29,7 @@ const ProjectViewTabs = () => {
                             title="test title"
                             text="test text"
                             buttonText="test button"
+                            href="test"
                         />
                         <ProjectCard
                             id="bootcamp"
@@ -33,6 +37,7 @@ const ProjectViewTabs = () => {
                             title="test title"
                             text="test text"
                             buttonText="test button"
+                            href="test"
                         />
                     </CardDeck>
                 </Tab>
